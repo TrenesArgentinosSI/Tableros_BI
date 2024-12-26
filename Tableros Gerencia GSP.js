495c5bdf62251795d4068c37af7b465eb205ebca
TrenesArgentinosSI <seguridadinformatica@trenesargentinos.gob.ar>
2024-12-26 14:09:14 -0300

    Update Tableros Gerencia GSP.js

--- Tableros Gerencia GSP.js
+++ Tableros Gerencia GSP.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tableros Gerencia GSP
 // @namespace    http://tampermonkey.net/
-// @version      1.11
+// @version      1.12
 // @description  Rotar enlaces con opción de pausa y reanudación mediante botones visibles, con contador de tiempo fluido.
 // @author       Tú
 // @match        https://app.powerbi.com/*
@@ -12,12 +12,12 @@
     'use strict';
 
     const links = [
-"https://app.powerbi.com/links/Ma76FMCl7w?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare&bookmarkGuid=25945810-27a5-424d-abe3-170829e396f9",
-        "https://app.powerbi.com/links/zK1Wg0oyeE?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare&bookmarkGuid=9667169b-d46c-4cf1-b045-354ac011ade8",
-        "https://app.powerbi.com/links/JDVlq5w5Om?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare&bookmarkGuid=866bb75a-f0dc-41f2-8a56-f7dbaac24f41",
-        "https://app.powerbi.com/links/3Oq9_0RLzY?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare",
-        "https://app.powerbi.com/links/zk_fpsVM1Y?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare&bookmarkGuid=c36b4544-9896-47b9-bd07-2971663a00fa",
-        "https://app.powerbi.com/links/GQsWlyo_kh?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare"
+"https://app.powerbi.com/links/Ma76FMCl7w?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare&bookmarkGuid=25945810-27a5-424d-abe3-170829e396f9?experience=power-bi&chromeless=true",
+        "https://app.powerbi.com/links/zK1Wg0oyeE?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare&bookmarkGuid=9667169b-d46c-4cf1-b045-354ac011ade8?experience=power-bi&chromeless=true",
+        "https://app.powerbi.com/links/JDVlq5w5Om?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare&bookmarkGuid=866bb75a-f0dc-41f2-8a56-f7dbaac24f41?experience=power-bi&chromeless=true",
+        "https://app.powerbi.com/links/3Oq9_0RLzY?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare?experience=power-bi&chromeless=true",
+        "https://app.powerbi.com/links/zk_fpsVM1Y?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare&bookmarkGuid=c36b4544-9896-47b9-bd07-2971663a00fa?experience=power-bi&chromeless=true",
+        "https://app.powerbi.com/links/GQsWlyo_kh?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare?experience=power-bi&chromeless=true"
         ];
 // URLs permitidas
 
