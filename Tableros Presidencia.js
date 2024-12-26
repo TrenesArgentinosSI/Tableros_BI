4e94b2c032e110d297c217763b565434005cec02
TrenesArgentinosSI <seguridadinformatica@trenesargentinos.gob.ar>
2024-12-26 13:58:59 -0300

    Update Tableros Presidencia.js

--- Tableros Presidencia.js
+++ Tableros Presidencia.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tableros Presidencia
 // @namespace    http://tampermonkey.net/
-// @version      1.10
+// @version      1.11
 // @description  Rotar enlaces con opción de pausa y reanudación mediante botones visibles, con contador de tiempo fluido.
 // @author       Tú
 // @match        https://app.powerbi.com/*
@@ -12,11 +12,14 @@
     'use strict';
 
     const links = [
-        "https://app.powerbi.com/groups/1c63bf60-4443-4c91-91e1-4b56098b309d/reports/f86773a1-278c-4bba-8b91-6f95b806f073/ReportSection1554dd7e053a66979e0b?experience=power-bi&chromeless=true",
-        "https://app.powerbi.com/groups/1c63bf60-4443-4c91-91e1-4b56098b309d/reports/f86773a1-278c-4bba-8b91-6f95b806f073/ReportSection770011af8ecda9a8b3eb?experience=power-bi&chromeless=true",
-        "https://app.powerbi.com/groups/1c63bf60-4443-4c91-91e1-4b56098b309d/reports/f86773a1-278c-4bba-8b91-6f95b806f073/ReportSectionf9fe292b4b0b2244a793?experience=power-bi&chromeless=true",
-        "https://app.powerbi.com/groups/1c63bf60-4443-4c91-91e1-4b56098b309d/reports/f86773a1-278c-4bba-8b91-6f95b806f073/ReportSection7b34db0f300b875c8c1b?experience=power-bi&chromeless=true"
-        ]; // URLs permitidas
+      "https://app.powerbi.com/links/Ma76FMCl7w?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare&bookmarkGuid=25945810-27a5-424d-abe3-170829e396f9",
+        "https://app.powerbi.com/links/zK1Wg0oyeE?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare&bookmarkGuid=9667169b-d46c-4cf1-b045-354ac011ade8",
+        "https://app.powerbi.com/links/JDVlq5w5Om?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare&bookmarkGuid=866bb75a-f0dc-41f2-8a56-f7dbaac24f41",
+        "https://app.powerbi.com/links/3Oq9_0RLzY?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare",
+        "https://app.powerbi.com/links/zk_fpsVM1Y?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare&bookmarkGuid=c36b4544-9896-47b9-bd07-2971663a00fa",
+        "https://app.powerbi.com/links/GQsWlyo_kh?ctid=9d6555ab-db4f-4ab0-8e7e-39efc4dc6730&pbi_source=linkShare"
+        ];
+// URLs permitidas
 
     // Verificar si la URL actual está en la lista de URLs permitidas
     const currentUrl = window.location.href;
